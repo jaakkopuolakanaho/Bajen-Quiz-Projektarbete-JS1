89a710fdcfecdf59eda43d1451a66b4c85002baf
Jaakko Puolakanahao <jarzo81@gmail.com>
2024-12-05 14:43:15 +0100

    update, new approach, new dark mode, wip

--- quiz.js
+++ quiz.js
@@ -1,122 +1,119 @@
-// Dark/Light theme function från DEV
-// https://dev.to/whitep4nth3r/the-best-lightdark-mode-theme-toggle-in-javascript-368f
 
-function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
-    if (localStorageTheme !== null) {
-      return localStorageTheme;
-    }
-  
-    if (systemSettingDark.matches) {
-      return "dark";
-    }
-  
-    return "light";
-  }
-
-  function updateButton({ buttonEl, isDark }) {
-    const newCta = isDark ? "Ändra till ljust tema" : "Ändra till mörkt tema";
-    buttonEl.innerText = newCta;
-  }
-
-  function updateThemeOnHtmlEl({ theme }) {
-    document.querySelector("html").setAttribute("data-theme", theme);
-  }
-
-  const button = document.querySelector("[data-theme-toggle]");
-  const localStorageTheme = localStorage.getItem("theme");
-  const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
-  let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
-  
-  updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
-  updateThemeOnHtmlEl({ theme: currentThemeSetting });
-  
-  button.addEventListener("click", (event) => {
-    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
-  
-    localStorage.setItem("theme", newTheme);
-    updateButton({ buttonEl: button, isDark: newTheme === "dark" });
-    updateThemeOnHtmlEl({ theme: newTheme });
-  
-    currentThemeSetting = newTheme;
-  }); 
-
-// Quiz
-
+//Array Data
 const quizArray = [
     {
+        type: "boolean",
         question: "1. Den 23 juni 2013 spelades den sista matchen på gamla Söderstadion och Kennedy Bakircioglu gjorde det sista målet någonsin på arenan.",
-        options: ["Sant", "Falskt"],
-        answer: "Sant",
-        type: "boolean"
+        answers: [
+            { option: " Sant ", correct: true },
+            { option: " Falskt ", correct: false }],
     },
     {
+        type: "radio",
         question: "2. Vem är huvudtränare för Hammarby IF herrlag (från och med 2024)?",
-        options: ["a) Zlatan Ibrahimović", "b) Nanne Bergstrand", "c) Kim Hellberg", "d) Henrik Rydström"],
-        answer: "c) Kim Hellberg",
-        type: "radio"
+        answers: [
+            { option: " a) Zlatan Ibrahimović ", correct: false },
+            { option: " b) Nanne Bergstrand ", correct: false },
+            { option: " c) Kim Hellberg ", correct: true },
+            { option: " d) Henrik Rydström ", correct: false }],
     },
     {
+        type: "checkbox",
         question: "3. Vilka av följande titlar har Hammarby IF vunnit?",
-        options: ["a) Svenska Cupen", "b) Nordic Nations League", "c) Allsvenskan", "d) Svenska Supercupen"],
-        answer: ["a) Svenska Cupen", "c) Allsvenskan"],
-        type: "checkbox"
+        answers: [
+            { option: " a) Svenska Cupen ", correct: true },
+            { option: " b) Nordic Nations League ", correct: false },
+            { option: " c) Allsvenskan ", correct: true },
+            { option: " d) Svenska Supercupen ", correct: false }],
     },
     {
+        type: "boolean",
         question: "4. Hammarby IF har aldrig spelat i någon lägre division än Allsvenskan.",
-        options: ["Sant", "Falskt"],
-        answer: "Falskt",
-        type: "boolean"
+        answers: [
+            { option: " Sant ", correct: false },
+            { option: " Falskt ", correct: true }],
     },
     {
+        type: "checkbox",
         question: "5. Vilka av följande spelare var med och vann SM-guld 2001 med Hammarby?",
-        options: ["a) Jörgen Pettersson", "b) Andreas Hermansson", "c) Mikael Andersson", "d) Martin Mutumba"],
-        answer: ["b) Andreas Hermansson", "c) Mikael Andersson"],
-        type: "checkbox"
+        answers: [
+            { option: " a) Jörgen Pettersson ", correct: false },
+            { option: " b) Andreas Hermansson ", correct: true },
+            { option: " c) Mikael Andersson ", correct: true },
+            { option: " d) Martin Mutumba ", correct: false }],
     },
     {
+        type: "boolean",
         question: "6. Den gamle Hammarbytränaren Sören Cratz vann SM-Guld med Hammarby och blev utsedd till 'Årets tränare', 'Årets idrottsledare', 'Årets ledare' samt 'Månadens stockholmare'.",
-        options: ["Sant", "Falskt"],
-        answer: "Sant",
-        type: "boolean"
+        answers: [
+            { option: " Sant ", correct: true },
+            { option: " Falskt ", correct: false }],
     },
     {
+        type: "radio",
         question: "7. Vilken av följande ramsor är inte en Bajen ramsa?",
-        options: ["a) Söder Bröder Glöder!", "b) Säg Oh ah Kennedy", "c) Forza Bajen, in med bollen, hey hey!", "d) You'll never walk alone"],
-        answer: "d) You'll never walk alone",
-        type: "radio"
+        answers: [
+            { option: " a) Söder Bröder Glöder ", correct: false },
+            { option: " b) Säg Oh ah Kennedy ", correct: false },
+            { option: " c) Forza Bajen, in med bollen, hey hey ", correct: false },
+            { option: " d) Du knallar aldrig ensam ", correct: true }],
     },
     {
+        type: "boolean",
         question: "8. Lennart 'Nacka' Skoglunds namn förknippas starkt med Hammarby IF. Han föddes på julafton 1929 och växte upp med sin familj på Katarina Bangata 42, där står det numera en staty till hans ära där Hammarbyare samlas varje julafton för att hedra hans minne.",
-        options: ["Sant", "Falskt"],
-        answer: "Sant",
-        type: "boolean"
+        answers: [
+            { option: " Sant ", correct: true },
+            { option: " Falskt ", correct: false }],
     },
     {
+        type: "radio",
         question: "9. Lennart 'Nacka' Skoglund är även känd under ett annat smeknamn, vilket?",
-        options: ["a) Den vajande majskolven", "b) Blixten från Söder", "c) Katarinas stjärna", "d) Trädgårdsmästaren"],
-        answer: "a) Den vajande majskolven",
-        type: "radio"
+        answers: [
+            { option: " a) Den vajande majskolven ", correct: true },
+            { option: " b) Blixten från Söder ", correct: false },
+            { option: " c) Katarinas stjärna ", correct: false },
+            { option: " d) Trädgårdsmästaren ", correct: false }],
     },
     {
+        type: "radio",
         question: "10. Hur stor del av Hammarby Fotbolls aktier äger Zlatan Ibrahimovic?",
-        options: ["a) Ingen", "b) Ca 5%", "c) Ca 10%", "d) Ca 25%."],
-        answer: "d) Ca 25%",
-        type: "radio"
+        answers: [
+            { option: " a) Ingen ", correct: false },
+            { option: " b) Ca 5% ", correct: false },
+            { option: " c) Ca 10% ", correct: false },
+            { option: " d) Ca 25% ", correct: true }],
     }
 ];
-  
-// Quiz
 
+//Dark/Light Mode
+const body = document.querySelector("body");
+const toggleBtn = document.querySelector(".toggleBtn");
+
+toggleBtn.addEventListener("click", toggleBackgroundBtn, false);
+
+let toggle = false;
+function toggleBackgroundBtn() {
+  body.style.backgroundColor = toggle ? "white" : "rgb(0, 136, 81)";
+
+  toggle = !toggle;
+}
+
+//HTML Element
+const questionDiv = document.querySelector("#question");
+const optionsDiv = document.querySelector("#options");
+const resultDiv = document.querySelector("#result");
+const reDoBtn = document.querySelector("#redoButton");
+const correctSpan = document.querySelector("#correct");
+const totalSpan = document.querySelector("#total");
+
+//Variables
 let questionIndex = 0;
-let answersArray = [];
+let correctAnswers = 0;
 
-function runQuiz(index) {
-    const questionDiv = document.querySelector("#question");
-    const optionsDiv = document.querySelector("#options");
-
-    const question = quizArray[index];
+//Fråga/Svar Function
+function showQuestion() {
+    const question = quizArray[questionIndex];
     
-    //Ny fråga, rensa gamla svaret
     questionDiv.innerHTML = `<p>${question.question}</p>`;
     optionsDiv.innerHTML = "";
 
@@ -128,8 +125,8 @@ function runQuiz(index) {
             input.type = "radio";
             input.name = `question-${index}`;
             input.value = option;
-            optionsDiv.appendChild(input);
-            optionsDiv.appendChild(label);
+            optionsDiv.append(input);
+            optionsDiv.append(label);
         });
     } else if (question.type === "radio") {
         question.options.forEach(option => {
@@ -139,8 +136,8 @@ function runQuiz(index) {
             input.type = "radio";
             input.name = `question-${index}`;
             input.value = option;
-            optionsDiv.appendChild(input);
-            optionsDiv.appendChild(label);
+            optionsDiv.append(input);
+            optionsDiv.append(label);
         });
     } else if (question.type === "checkbox") {
         question.options.forEach(option => {
@@ -150,10 +147,11 @@ function runQuiz(index) {
             input.type = "checkbox";
             input.name = `question-${index}`;
             input.value = option;
-            optionsDiv.appendChild(input);
-            optionsDiv.appendChild(label);
+            optionsDiv.append(input);
+            optionsDiv.append(label);
         }); 
         
+        //This. Varnar för att välja mer är 2 alternativ.
         document.querySelectorAll("input[type='checkbox']").forEach(el => {
             el.addEventListener('change', function() {
                 if (el.closest("#options").querySelectorAll('input:checked').length > 2) {
@@ -166,13 +164,57 @@ function runQuiz(index) {
     };
 
     
-};
 
-const nextBtn = document.querySelector("#nextBtn");
+    answerBtn.innerHTML = 
+    `<p>${question.question}</p>
+        <ul>
+            ${question.answers.map(answer => `
+                <li>
+                    <button id="answerBtn">${answer.option}</button>
+                </li>
+            `).join("")}
+        </ul>
+    `;
 
-nextBtn.addEventListener('click', () => {
+    const answerBtn = document.querySelectorAll("#answerBtn");
+    answerBtn.forEach(button => {
+        button.addEventListener("click", checkAnswer);
+    }); 
+}
+
+//Kolla svar, uppdatera variabler
+function checkAnswer(e) {
+    const selectedButton = e.target;
+    const isCorrect = quizArray[questionIndex].answers.find(answer => answer.option === selectedButton.textContent).correct;
+    if (isCorrect) {
+        correctAnswers++;
+    }
+    questionIndex++;
+    if (questionIndex === quizArray.length) {
+        showResults();
+    } else {
+        showQuestion();
+    }
+}
+
+//Resultat Function
+function showResults() {
+    questionDiv.style.display = "none";
+    resultDiv.style.display = "block";
+    correctSpan.textContent = correctAnswers;
+    totalSpan.textContent = quizArray.length;
+}
+
+//Starta om quiz
+reDoBtn.addEventListener("click", () => {
+    questionIndex = 0
+
+    // questionIndex = 0;
+    // correctAnswers = 0;
+    // resultDiv.style.display = "none";
+    // questionDiv.style.display = "block";
+    // showQuestion();
 
 });
 
-// Start the quiz
-runQuiz(questionIndex);
+showQuestion();
